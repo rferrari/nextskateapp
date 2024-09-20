996cbf62fa1e25ca64eb4af86628e64595fb0417
RFerrari <rferrariwd@gmail.com>
2024-09-20 16:13:22 -0300

    fix: profile cards css cleanup

--- src/components/Profile/profileCard.tsx
+++ src/components/Profile/profileCard.tsx
@@ -24,7 +24,6 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
     const userMetadata = useMemo(() => JSON.parse(user.json_metadata || '{}'), [user.json_metadata]);
     const userPostingMetadata = useMemo(() => JSON.parse(user.posting_json_metadata || '{}'), [user.posting_json_metadata]);
 
-//    const userLevel = userMetadata.extensions?.level || 0;
     const gnarsBalance = useGnarsBalance(userMetadata.extensions?.eth_address || '');
     const { hivePower } = useHiveBalance(user);
 
@@ -32,7 +31,7 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
     const [userVideoParts, setUserVideoParts] = useState(userMetadata.extensions?.video_parts?.length || 0);
 
     // const userLevel = userMetadata.extensions?.level || 0;
-    const userLevel = 0;    //debug lvl
+    const userLevel = 3;    //debug lvl
 
     const handleClick = () => {
         setIsFlipped(!isFlipped);
@@ -55,6 +54,7 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
                 onClose={onClose} user={user} />}
 
         <Flex justify="center" direction="column" width="full" height="full">
+
             {/* Profile Card Container */}
             <Box id="containerCardProfile"
                 border="2px solid white"
@@ -65,26 +65,37 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
                 onClick={handleClick}
                 zIndex={0}
                 style={{
-                    perspective: '1000px',
+                    transformStyle: 'preserve-3d',
+                    perspective: '2000px',
                     width: '310px',
                     height: '550px',
                 }}
             >
+
                 {/* Profile Card Front Side */}
-                <Card id="front-side-card"        className={`card-profile`}
-                      border="2px solid white"    size="sm"
-                      color="white"               bg={'transparent'}
-                      p={2}                       borderRadius="20px"
-                      position={'absolute'}
-                      opacity={isFlipped ? '0' : '1'}
-                      zIndex={isFlipped ? '0' : '1'}
+                <Card id="front-side-card" 
+                    className={`front-card-profile`}
+                    border="2px solid white" 
+                    borderRadius="20px"
+                    bg={'transparent'} 
+                    color="white"
+                    position={'absolute'}
+                    transition="0.6s"
+                    opacity={isFlipped ? '0' : '1'}
+                    zIndex={isFlipped ? '0' : '2'}
+                    style={{
+                        // backfaceVisibility: 'visible',  /* Changed to visible */
+                        // perspective: 'none',            /* Removed perspective */
+                    }}
+
                 >
-                    <CardHeader borderTopRadius="10px" 
-                        textAlign="center" 
-                        bg="transparent" 
+                    <CardHeader id='frontSideHeader'
+                        borderTopRadius="10px"
+                        textAlign="center"
+                        bg="transparent"
                         p={2}
-                        style={{backfaceVisibility: 'hidden',}}
-                        >
+                        style={{ backfaceVisibility: 'hidden', }}
+                    >
                         <HStack justifyContent="space-between">
                             <HStack justifyContent="flex-start">
                                 <Text fontWeight="bold" fontSize="18px"
@@ -101,12 +112,13 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
                         </HStack>
                     </CardHeader>
 
-                    <CardBody bg="transparent"
-                              style={{backfaceVisibility: 'hidden',}}
+                    <CardBody id='frontSideBody'
+                        bg="transparent"
+                        style={{ backfaceVisibility: 'hidden', }}
                     >
                         <VStack>
                             <Center>
-                                <Box borderRadius={14} border="3px solid black">
+                                <Box borderRadius={14} border="3px solid white">
                                     <UserAvatar hiveAccount={user} borderRadius={10} boxSize={150} />
                                 </Box>
                             </Center>
@@ -114,8 +126,8 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
 
                         <VStack w="100%" marginTop={5}>
                             <Box border="1px solid white"
-                                 w={230} borderRadius="10px" p={2}
-                                 bg="rgba(0, 0, 0, 0.8)">
+                                w={230} borderRadius="10px" p={2}
+                                bg="rgba(0, 0, 0, 0.8)">
                                 <HStack justify="space-between">
                                     <HStack>
                                         <Image src="/logos/hp_logo.png" alt="Logo" boxSize="20px" />
@@ -145,10 +157,11 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
                         </VStack>
                     </CardBody>
 
-                    <CardFooter fontSize="16px" fontWeight="bold" 
-                                color="white" mb={-2} 
-                                style={{backfaceVisibility: 'hidden',}}
-                                >
+                    <CardFooter id='frontSideFooter'
+                        fontSize="16px" fontWeight="bold"
+                        color="white" mb={-2}
+                        style={{ backfaceVisibility: 'hidden', }}
+                    >
                         <VStack w="100%">
                             <Flex justify="center">
                                 <div style={{ zIndex: 10 }}>
@@ -201,43 +214,41 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
                 </Card>
 
                 {/* Back side of the card */}
-                <Card id="back-side-card"    className={`card-profile`}
-                    border="2px solid white" borderRadius="20px"
-                    bg={'transparent'}
-                    transition="0.6s"
+                <Card id="back-side-card" className={`back-card-profile`}
+                    position={'absolute'}
+                    color="white"                   //need to be here for chakra
+                    bg={'transparent'}              //need to be here for chakra
                     opacity={isFlipped ? '1' : '0'}
                     zIndex={isFlipped ? '2' : '0'}
                 >
-                    <CardHeader transform={'rotateY(180deg)'}
-                        color="white"
-                        style={{
-                            backgroundImage: `url(${nextImage(userPostingMetadata?.profile?.cover_image || `https://images.ecency.com/webp/u/${user.name}/cover/small`)})`,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center',
-                            borderBottom: '1px solid white',
-                            borderRadius: '10px',
-                            textAlign: 'center',
-                            backgroundColor: 'gray.900',
-                            padding: 2,
-                        }}
-                    >
+                    <CardHeader id='backSideHeader'
+                            style={{
+                                backgroundImage: `url(${nextImage(userPostingMetadata?.profile?.cover_image || `https://images.ecency.com/webp/u/${user.name}/cover/small`)})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                borderBottom: '1px solid white',
+                                borderRadius: '10px',
+                                textAlign: 'center',
+                                backgroundColor: 'gray.900',
+                                padding: 2,
+                            }}>
                         <HStack justify="center">
                             <UserAvatar hiveAccount={user} borderRadius={100} boxSize={20} />
                             <Text size="md" color="white"></Text>
                         </HStack>
                     </CardHeader>
 
-                    <CardBody transform={'rotateY(180deg)'}
+                    <CardBody id='backSideBody'
                         textAlign="center"
                         width="100%"
                         height="100%"
                         borderRadius="20px"
                         padding={0}
                         marginTop={5}
-                        >
+                    >
                         <HStack justify="center">
-                            <Box border="1px solid white" w={230} borderRadius="10px" p={2}
-                                color={'white'}
+                            <Box border="1px solid white" w={230} borderRadius="10px"
+                                p={2}
                                 bg="rgba(0, 0, 0, 0.8)"
                             >
                                 {userPostingMetadata.profile?.about || "I'm too lazy to write a bio."}
@@ -245,8 +256,10 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
                         </HStack>
 
                         <HStack justify="center">
-                            <Box border="1px solid white" w={230} borderRadius="10px" p={2} marginTop={5}
-                                color={'white'}
+                            <Box border="1px solid white" borderRadius="10px" 
+                                w={230}
+                                p={2}
+                                marginTop={5}
                                 bg="rgba(0, 0, 0, 0.8)"
                             >
                                 <Text fontWeight="bold" fontSize="18px">
@@ -256,12 +269,12 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
                         </HStack>
                     </CardBody>
 
-                    <CardFooter transform={'rotateY(180deg)'}>
+                    <CardFooter id='backSideFooter'>
                     </CardFooter>
                 </Card>
 
             </Box>
-        </Flex >
+        </Flex>
     </>);
 }
 
